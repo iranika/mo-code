a8774d3023617cbf704f75a2772f6c6816763a17
iranika <7370486+iranika@users.noreply.github.com>
2019-09-20 02:00:52 +0900

    update: auto updated 4komaData by yurika

--- 4komaData.js
+++ 4komaData.js
@@ -221,4 +221,5 @@ pageData = [
 ,{"Title": "没収", "ImagesUrl": ["https://mo-code.iranika.info/4koma/220-1.jpg", "https://mo-code.iranika.info/4koma/220-2.jpg", "https://mo-code.iranika.info/4koma/220-3.jpg", "https://mo-code.iranika.info/4koma/sp.jpg"]}
 ,{"Title": "都会デビュー", "ImagesUrl": ["https://mo-code.iranika.info/4koma/221-1.jpg", "https://mo-code.iranika.info/4koma/221-2.jpg", "https://mo-code.iranika.info/4koma/221-3.jpg", "https://mo-code.iranika.info/4koma/sp.jpg"]}
 ,{"Title": "洋画", "ImagesUrl": ["https://mo-code.iranika.info/4koma/222-1.jpg", "https://mo-code.iranika.info/4koma/222-2.jpg", "https://mo-code.iranika.info/4koma/sp.jpg"]}
+,{"Title": "稲刈り", "ImagesUrl": ["https://mo-code.iranika.info/4koma/223-1.jpg", "https://mo-code.iranika.info/4koma/sp.jpg"]}
 ]

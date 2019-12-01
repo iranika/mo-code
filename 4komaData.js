e831dbe7f630c5ae5529f50b3fe7bf113364bdb7
iranika <7370486+iranika@users.noreply.github.com>
2019-12-02 02:00:53 +0900

    update: auto updated 4komaData by yurika

--- 4komaData.js
+++ 4komaData.js
@@ -226,5 +226,5 @@ pageData = [
 ,{"Title": "秘訣", "ImagesUrl": ["https://mo-code.iranika.info/4koma/225-1.jpg", "https://mo-code.iranika.info/4koma/225-2.jpg", "https://mo-code.iranika.info/4koma/225-3.jpg", "https://mo-code.iranika.info/4koma/sp.jpg"]}
 ,{"Title": "吸血鬼", "ImagesUrl": ["https://mo-code.iranika.info/4koma/226-1.jpg", "https://mo-code.iranika.info/4koma/226-2.jpg", "https://mo-code.iranika.info/4koma/226-3.jpg", "https://mo-code.iranika.info/4koma/sp.jpg"]}
 ,{"Title": "無駄遣い", "ImagesUrl": ["https://mo-code.iranika.info/4koma/227-1.jpg", "https://mo-code.iranika.info/4koma/227-2.jpg", "https://mo-code.iranika.info/4koma/sp.jpg"]}
-,{"Title": "影響", "ImagesUrl": ["https://mo-code.iranika.info/4koma/228-1.jpg", "https://mo-code.iranika.info/4koma/sp.jpg"]}
+,{"Title": "影響", "ImagesUrl": ["https://mo-code.iranika.info/4koma/228-1.jpg", "https://mo-code.iranika.info/4koma/228-2.jpg", "https://mo-code.iranika.info/4koma/sp.jpg"]}
 ]

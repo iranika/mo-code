6354bf20f3240231a6cde071c086a910e06d36cd
iranika <7370486+iranika@users.noreply.github.com>
2020-01-10 16:09:41 +0900

    del: dead code.

--- js/michikusa-viewer.js
+++ js/michikusa-viewer.js
@@ -10,9 +10,6 @@ const viewer = (()=>{
     },
     conf : { //Viewer関連の設定情報
       isAutoShowMore : true,
-      tweetText: encodeURI("ここすき\n#みちくさびゅあー"),
-      tweetHashTag: encodeURI(""),
-      tweetUrl: encodeURI("http://iranika.github.io/mo-code/")
     },
     pageData : pageData,
     page : { //pageに関する変数
@@ -53,8 +50,8 @@ const viewer = (()=>{
       }
     },
     isLastPage : (pageNum)=>{
-      if (viewer.page.length == pageNum){ return true}
-      else { return false}
+      if (viewer.page.length == pageNum){ return true }
+      else { return false }
     },
     initShowImage : (pageNum)=>{
       viewer.elem.view.innerHTML = ""
@@ -68,7 +65,7 @@ const viewer = (()=>{
       viewer.initShowImage(viewer.page.length)
     },
     showHiddenImage : ()=>{
-      var elems = document.querySelectorAll(".hidden-image")
+      const elems = document.querySelectorAll(".hidden-image")
       if (elems.length != 0){
         [].forEach.call(elems,(elem) => {
           elem.className = elem.className.replace("hidden-image", "")
@@ -80,7 +77,7 @@ const viewer = (()=>{
       viewer.pageData[pageNum].ImagesUrl.forEach(img_url => {
         viewer.elem.view.innerHTML += `<img class="hidden-image" src="${img_url}">`
       })
-      let tw_text = encodeURI(`第${pageNum +1}話「${viewer.pageData[pageNum].Title}」`)
+      const tw_text = encodeURI(`第${pageNum +1}話「${viewer.pageData[pageNum].Title}」`)
       viewer.elem.view.innerHTML += `<a class="tweetbtn hidden-image" href="https://twitter.com/intent/tweet?hashtags=%E9%81%93%E8%8D%89%E5%B1%8B&original_referer=https%3A%2F%2Fpublish.twitter.com%2F%3FbuttonText%3D%25E3%2581%2593%25E3%2581%2593%25E3%2581%2599%25E3%2581%258D%25EF%25BC%2581%25EF%25BC%2581%2523%25E3%2581%25BF%25E3%2581%25A1%25E3%2581%258F%25E3%2581%2595%25E3%2581%25B3%25E3%2582%2585%25E3%2581%2582%25E3%2583%25BC%26buttonType%3DTweetButton%26buttonUrl%3Dhttps%253A%252F%252Firanika.github.io%252Fmo-code%252F%252310%26dnt%3D1%26lang%3Dja%26widget%3DButton&ref_src=twsrc%5Etfw&text=${tw_text}&tw_p=tweetbutton&url=https%3A%2F%2Firanika.github.io%2Fmo-code%2F%23${pageNum +1}"><span class="label">ツイートする</span></a>`
     }
   }
@@ -96,7 +93,7 @@ window.onload = ()=>{
     viewer.initShowImage(parseInt(hash) -1) //話数と合わせるために-1する
   }
   viewer.addTitleToElement(viewer.elem.rightMenu)
-  var triggerMargin = 5
+  const triggerMargin = 5
   //sub funcions
   window.addEventListener("scroll", function(){
     //console.log(elem.footer.getBoundingClientRect().bottom)
